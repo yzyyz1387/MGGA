79c1f3913ea11b94adb47c1758e633e2bd8175a5
yzyyz1387 <1796031384@qq.com>
2022-12-12 00:18:42 +0800

    :sparkles: 发布到greasy fork

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
     // @namespace    http://tampermonkey.net/
     // @version      0.1
     // @description  自定义 GitHub css样式
-    // @author       github@yzyyz1387
+    // @author       幼稚园园长 github@yzyyz1387
     // @match        https://github.com/*
     // @resource     data https://cdn.staticaly.com/gh/yzyyz1387/MGGA/master/style_p.css?dev=1
     // @grant        GM_addStyle
@@ -12,11 +12,6 @@
     // @run-at document-start
     // ==/UserScript==
 
-    // 2022/12/11
-    // index.js
-    // Author: 幼稚园园长
-    // SoftWare: WebStorm
-    /* global hljs, encodeToGb2312 */
     (function () {
         'use strict';
         let css = GM_getResourceText("data");
