61affcabc42849f8ca06adefd9d48ae94416f50a
yzyyz1387 <1796031384@qq.com>
2022-12-11 10:57:38 +0800

    :sparkles: 调整样式

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-// 2022/12/11
-// personal_gh_style.js
-// Author: 幼稚园园长
-// SoftWare: WebStorm
-// @name         GitHub personal style
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  自定义 GitHub css样式
-// @author       You
-// @match        https://github.com/*
-// @grant        none
-// ==/UserScript==
-(function () {
-    'use strict';
+    // ==UserScript==
+    // @name         GitHub personal style
+    // @namespace    http://tampermonkey.net/
+    // @version      0.1
+    // @description  自定义 GitHub css样式
+    // @author       github@yzyyz1387
+    // @match        https://github.com/*
+    // @resource     data https://cdn.staticaly.com/gh/yzyyz1387/MGGA/master/style_p.css
+    // @grant        GM_addStyle
+    // @grant        GM_getResourceText
+    // @grant unsafeWindow
+    // @run-at document-start
+    // ==/UserScript==
 
-    function loadStyles(url) {
-        const link = document.createElement("link");
-        link.type = "text/css";
-        link.rel = "stylesheet";
-        link.href = url;
-        document.getElementsByTagName("head")[0].appendChild(link);
-    }
+    // 2022/12/11
+    // index.js
+    // Author: 幼稚园园长
+    // SoftWare: WebStorm
+    /* global hljs, encodeToGb2312 */
+    (function () {
+        'use strict';
+        let css = GM_getResourceText("data");
+        if (typeof GM_addStyle !== "undefined") {
+            GM_addStyle(css);
+        } else {
+            let styleNode = document.createElement("style");
+            styleNode.appendChild(document.createTextNode(css));
+            (document.querySelector("head") || document.documentElement).appendChild(styleNode);
+        }
+        hljs.highlightAll();
+    })();
 
-    loadStyles("https://cdn.staticaly.com/gh/yzyyz1387/MGGA/master/style_p.css")
-})();
 
